edd971b80d0bac4a0f919b6d88552d3634b5d0d0
Michael Vilabrera <m.vilabrera@gmail.com>
2015-09-09 18:08:24 -0400

    initial commit

--- PlayingWithGeocoding/ViewController.swift
+++ PlayingWithGeocoding/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    var labelTop: UILabel!
+    var textFieldCenter: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        makeLayout()
     }
 
+    func makeLayout() {
+        let view1 = UIView()
+        view1.translatesAutoresizingMaskIntoConstraints = false
+        view1.backgroundColor = UIColor.blueColor()
+        
+        let view2 = UIView()
+        view2.translatesAutoresizingMaskIntoConstraints = false
+        view2.backgroundColor = UIColor.greenColor()
+        
+        view.addSubview(view1)
+        view.addSubview(view2)
+        
+        let viewsDictionary = ["view1":view1, "view2": view2, "labelTop":labelTop, "textFieldCenter":textFieldCenter]
+        let metricsDictionary = ["view1Height":50.0, "view2Height":30.0, "viewWidth":100.0]
+        
+        let view1_constraint_H:Array = NSLayoutConstraint.constraintsWithVisualFormat("H:[view1(viewWidth)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metricsDictionary, views: viewsDictionary)
+        let view1_constraint_V:Array = NSLayoutConstraint.constraintsWithVisualFormat("V:[view1(view1Height)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metricsDictionary, views: viewsDictionary)
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
